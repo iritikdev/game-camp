a79193b7bdcb2c82b5bd6d812ff71b1b645d519e
Ritik <iritikdev@gmail.com>
2023-03-22 15:06:16 +0530

    Build a Resposive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem gridArea={"nav"} bgColor="antiquewhite">
+        Nav
+      </GridItem>
+
+      <Show above="lg">
+        <GridItem gridArea={"aside"} bgColor="Yellow">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem gridArea={"main"} bgColor="gold">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
