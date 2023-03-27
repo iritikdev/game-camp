8314468ff170186b8dd588acf352b6e3ca87c2a2
Ritik <iritikdev@gmail.com>
2023-03-27 22:56:38 +0530

    change dark mode icon

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,18 +1,29 @@
-import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
-
+import {
+  Button,
+  HStack,
+  Switch,
+  Text,
+  useColorMode,
+  Icon,
+} from "@chakra-ui/react";
+import { MdDarkMode, MdOutlineDarkMode } from "react-icons/md";
 interface Props {}
 
 function ColorModeSwitch({}: Props) {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <HStack>
-      <Switch
-        isChecked={colorMode === "dark"}
-        colorScheme="green"
-        onChange={toggleColorMode}
-      />
-      <Text>Dark Mode</Text>
-    </HStack>
+    <Button onClick={toggleColorMode} variant="ghost">
+      {colorMode === "dark" && <MdDarkMode size={"24"} />}
+      {colorMode === "light" && <MdOutlineDarkMode size={"24"} />}
+    </Button>
+    // <HStack>
+    //   <Switch
+    //     isChecked={colorMode === "dark"}
+    //     colorScheme="green"
+    //     onChange={toggleColorMode}
+    //   />
+    //   <Text>Dark Mode</Text>
+    // </HStack>
   );
 }
 
